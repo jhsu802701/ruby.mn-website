e2c6a1c15570bf57c977871acf84330b365caaba
Jason Hsu <jhsu802701@jasonhsu.com>
2017-02-16 02:08:27 +0000

    Added outlining to all.sh

--- all.sh
+++ all.sh
@@ -11,3 +11,18 @@ echo '-------------------------------------'
 echo "sh test_code.sh > $FILE_LOG_TEST_CODE"
 sh test_code.sh > $FILE_LOG_TEST_CODE
 echo 'The Gemsurance Report is at log/gemsurance_report.html .'
+
+FILE_LOG_OUTLINE='log/all-outline.log'
+echo '---------------------------------'
+echo "sh outline.sh > $FILE_LOG_OUTLINE"
+sh outline.sh > $FILE_LOG_OUTLINE
+
+d_mo='log/diagram-models.jpg'
+d_co='log/diagram-controllers.jpg'
+d_gems='log/diagram-gems.jpg'
+
+echo 'outline.sh OUTPUT FILES:'
+echo $d_mo
+echo $d_co
+echo $d_gems
+echo 'Directory trees are in the notes directory.'
